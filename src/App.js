0428c6ed281d29aa240ca814506edec0309a2216
Tasnim199 <tasnim.zidi1999@gmail.com>
2023-03-04 12:31:11 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbarr from "./Navbarr";
+import Carrousssel from "./Carrousssel";
+import Bouttonn from "./Boutonn";
+import Alertsss from "./Alertsss";
+import Formulairee from "./Formulairee";
+function App() {
+  return (
+    <div className="App">
+      <Navbarr />
+      <Carrousssel />
+      <Bouttonn />
+      <Formulairee />
+      <Alertsss />
+    </div>
+  );
+}
+
+export default App;
